620192c874b5da4a9d5b5ef6cfe4d15adaca982f
119 <10665739+one19@users.noreply.github.com>
2021-06-24 10:30:44 +0000

    :sparkles: init commit!

--- index.mjs
+++ index.mjs
@@ -1,6 +1,45 @@
-#!/usr/bin/env zx
+import fs from 'fs';
+import { $ } from 'zx';
+import path from 'path';
+import util from 'util';
+import { fileURLToPath } from 'url';
 
-/* eslint-disable no-undef */
-await $`fortune | cowsay | lolcatjs`;
+const writeFile = util.promisify(fs.writeFile);
+const fileUrl = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileUrl);
+const [, , renameArg] = process.argv;
 
-await $`git branch --show-current`;
+if (!renameArg) throw new Error('Must provide a name to replace (things) with.');
+
+export const modifyFile = async (filePath, rename = renameArg) => {
+  const contents = fs.readFileSync(filePath, 'utf8');
+  writeFile(
+    filePath,
+    contents
+      .replace(/thing/g, rename)
+      .replace(/Thing/g, `${rename[0].toUpperCase()}${rename.slice(1)}`)
+  );
+};
+
+export const recurseFolders = async (directoryFilePath = dirName) => {
+  const directoryFiles = fs
+    .readdirSync(directoryFilePath)
+    .filter((e) => !/^(.git|node_modules)$/.test(e));
+
+  const [folders, files] = directoryFiles.reduce(
+    ([foldersSum, filesSum], fileName) => {
+      const filePath = `${directoryFilePath}/${fileName}`;
+      const isFolder = fs.lstatSync(filePath).isDirectory();
+
+      if (isFolder) return [[...foldersSum, filePath], filesSum];
+      return [foldersSum, [...filesSum, filePath]];
+    },
+    [[], []]
+  );
+
+  await Promise.all(folders.map(recurseFolders));
+  await Promise.all(files.map(modifyFile));
+};
+
+await recurseFolders();
+await $`npm uninstall solosis-codemod`;
